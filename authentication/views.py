0b92c386248e807288151df1544fc25c845b7a79
etali-m <etalijunior15@gmail.com>
2022-09-22 03:50:52 +0100

    genericView

--- authentication/views.py
+++ authentication/views.py
@@ -4,30 +4,7 @@ from . import forms
 from django.contrib.auth import login, authenticate, logout #les methodes pour l'authentification et le connexion
 from django.views.generic import View #Nécessaire pour utiliser les vues basées sur les classes
 
-#the class view for the login page
-class LoginPageView(View):
-    template_name = 'authentication/login.html'
-    form_class = forms.LoginForm
-
-    def get(self, request):
-        form = self.form_class()
-        message = ''
-        return render(request, self.template_name, context={'form': form, 'message':message})
-
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            user = authenticate(
-                username = form.cleaned_data['username'],
-                password = form.cleaned_data['password'],
-            )
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-        message = 'Login failed!'
-        return render(request, self.template_name, context={'form':form, 'message':message})
-
+ 
 #la vue pour la deconnexion d'un utilisateur
 def logout_user(request):
     logout(request)
--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -129,3 +129,4 @@ AUTH_USER_MODEL = 'authentication.User'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = LOGIN_URL
